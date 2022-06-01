bba6af50eb86c9ba220cbe6949eb2f5036255d10
bimashazaman <bimashazaman1234@gmail.com>
2022-06-01 15:53:08 +0600

    first commit

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,4 +1,4 @@
-const monsterHealthBar = document.getElementById('monster-health'); 
+const monsterHealthBar = document.getElementById('monster-health');
 const playerHealthBar = document.getElementById('player-health');
 const bonusLifeEl = document.getElementById('bonus-life');
 
@@ -7,16 +7,12 @@ const strongAttackBtn = document.getElementById('strong-attack-btn');
 const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
 
-
 function adjustHealthBars(maxLife) {
-
   monsterHealthBar.max = maxLife;
   monsterHealthBar.value = maxLife;
-
   playerHealthBar.max = maxLife;
   playerHealthBar.value = maxLife;
-  
-} 
+}
 
 function dealMonsterDamage(damage) {
   const dealtDamage = Math.random() * damage;
