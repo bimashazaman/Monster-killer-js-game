50c4d1d7809ee3778e9d9c3169c383f2c6a16e3b
bimashazaman <bimashazaman1234@gmail.com>
2022-06-01 14:51:48 +0600

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,17 @@
+const ATTACK_VALUE = 10;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler(params) {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+ 
+}
+
+attackBtn.addEventListener('click', attackHandler);
+
