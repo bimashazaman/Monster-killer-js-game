4d4e3800b6dd28802933f22daf8b61e7a2e88765
bimashazaman <bimashazaman1234@gmail.com>
2022-06-02 14:15:47 +0600

    hkxhsoxh

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -15,9 +15,9 @@ function adjustHealthBars(maxLife) {
 }
 
 function dealMonsterDamage(damage) {
-  const dealtDamage = Math.random() * damage; // 0.5 * 10 = 5
-  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage; // 100 - 5 = 95
-  return dealtDamage; // 5
+  const dealtDamage = Math.random() * damage;
+  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
+  return dealtDamage;
 }
 
 function dealPlayerDamage(damage) {
